66234294d87f17b8d2ef352d21198112cbeb7706
orhun <orhunparmaksiz@gmail.com>
2020-04-16 21:13:49 +0300

    feat: Add Rect struct and get_rect method to window module

--- src/x11/window.rs
+++ src/x11/window.rs
@@ -1,6 +1,41 @@
 use x11::xlib;
 
+#[derive(Debug)]
+pub struct Rect {
+	pub x: i32,
+	pub y: i32,
+	pub width: u32,
+	pub height: u32,
+}
+
 pub struct Window {
 	pub xid: usize,
 	pub display: *mut xlib::Display,
+}
+
+impl Window {
+	pub fn get_rect(&self) -> Rect {
+		let mut root: xlib::Window = 0;
+		let (mut x, mut y, mut width, mut height, mut border_width, mut depth) =
+			(0, 0, 0, 0, 0, 0);
+		unsafe {
+			xlib::XGetGeometry(
+				self.display,
+				self.xid as u64,
+				&mut root,
+				&mut x,
+				&mut y,
+				&mut width,
+				&mut height,
+				&mut border_width,
+				&mut depth,
+			);
+		}
+		Rect {
+			x,
+			y,
+			width,
+			height,
+		}
+	}
 }
