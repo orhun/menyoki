0ae1be679362c6972a944a3ac38261d6c4c54436
orhun <orhunparmaksiz@gmail.com>
2020-04-18 02:01:37 +0300

    feat: Remove origin method from Rect and derive Default trait

--- src/image/mod.rs
+++ src/image/mod.rs
@@ -1,22 +1,11 @@
 pub mod gif;
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, Default)]
 pub struct Rect {
 	pub x: i32,
 	pub y: i32,
 	pub width: u32,
 	pub height: u32,
-}
-
-impl Rect {
-	pub fn origin(&self) -> Self {
-		Rect {
-			x: 0,
-			y: 0,
-			width: self.width,
-			height: self.height,
-		}
-	}
 }
 
 #[derive(Debug)]
