87ba2ae9b50d66578c6475815eb57796054ff9f3
orhun <orhunparmaksiz@gmail.com>
2020-04-19 15:55:44 +0300

    doc: Add function comments to parse_args in util module

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,10 @@
 use clap::App;
 
+/**
+ * Parse command line arguments.
+ *
+ * @return ArgMatches
+ */
 pub fn parse_args() -> clap::ArgMatches<'static> {
 	App::new(env!("CARGO_PKG_NAME"))
 		.version(env!("CARGO_PKG_VERSION"))
