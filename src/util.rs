624ab3c91637fe5e1d53363e39b977a4bad5614d
orhun <orhunparmaksiz@gmail.com>
2020-04-19 15:57:09 +0300

    refactor: Use ArgMatches implicitly for parse_args

--- src/util.rs
+++ src/util.rs
@@ -1,11 +1,11 @@
-use clap::App;
+use clap::{App, ArgMatches};
 
 /**
  * Parse command line arguments.
  *
  * @return ArgMatches
  */
-pub fn parse_args() -> clap::ArgMatches<'static> {
+pub fn parse_args() -> ArgMatches<'static> {
 	App::new(env!("CARGO_PKG_NAME"))
 		.version(env!("CARGO_PKG_VERSION"))
 		.author(env!("CARGO_PKG_AUTHORS"))
