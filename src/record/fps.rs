7de86bdd29ad8317204cdfba3a7a80424eb3e5c3
orhun <orhunparmaksiz@gmail.com>
2020-04-21 17:59:44 +0300

    refactor: Use TimeUnit enum for fps calculations

--- src/record/fps.rs
+++ src/record/fps.rs
@@ -1,34 +1,41 @@
-use std::time;
 use std::thread;
+use std::time;
+
+#[derive(Debug)]
+pub enum TimeUnit {
+	Nanosecond,
+	Millisecond,
+}
 
 #[derive(Debug)]
 pub struct FpsClock {
-    last_tick_time: time::Instant,
-    fps: u32,
-    fps_in_nanos: f32
+	fps: u32,
+	last_tick_time: time::Instant,
 }
 
 impl FpsClock {
-    pub fn new(fps: u32) -> FpsClock {
-        FpsClock {
-            last_tick_time: time::Instant::now(),
-            fps: fps,
-            fps_in_nanos: (1. / fps as f32) * 1_000_000_000.
-        }
-    }
+	pub fn new(fps: u32) -> FpsClock {
+		FpsClock {
+			fps,
+			last_tick_time: time::Instant::now(),
+		}
+	}
 
-    pub fn fps(&self) -> u32 {
-        self.fps
-    }
+	pub fn get_fps(&self, unit: TimeUnit) -> f32 {
+		match unit {
+			TimeUnit::Nanosecond => (1. / self.fps as f32) * 1_000_000_000.,
+			TimeUnit::Millisecond => (1. / self.fps as f32) * 1_000.,
+		}
+	}
 
-    pub fn tick(&mut self) -> f32 {
-        let t = self.last_tick_time.elapsed();
-        let total_nanos = t.as_secs() * 1_000_000_000 + t.subsec_nanos() as u64;
-        let diff = self.fps_in_nanos - (total_nanos as f32);
-        if diff > 0. {
-            thread::sleep(time::Duration::new(0, diff as u32))
-        };
-        self.last_tick_time = time::Instant::now();
-        diff
-    }
+	pub fn tick(&mut self) -> f32 {
+		let t = self.last_tick_time.elapsed();
+		let total_nanos = t.as_secs() * 1_000_000_000 + t.subsec_nanos() as u64;
+		let diff = self.get_fps(TimeUnit::Nanosecond) - (total_nanos as f32);
+		if diff > 0. {
+			thread::sleep(time::Duration::new(0, diff as u32))
+		};
+		self.last_tick_time = time::Instant::now();
+		diff
+	}
 }
