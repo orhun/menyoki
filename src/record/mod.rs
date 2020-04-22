2e9ad802b40f21e914b9ef6b51c672bc56eebbd9
orhun <orhunparmaksiz@gmail.com>
2020-04-22 15:16:49 +0300

    feat: Use the FPS time for frame delays

--- src/record/mod.rs
+++ src/record/mod.rs
@@ -37,22 +37,16 @@ impl Recorder {
 		get_image: impl Fn() -> Option<Image> + Sync + Send + 'static,
 	) -> RecordResult {
 		let recorder = Box::leak(Box::new(self));
-		let mut tick = 0.0;
 		let mut frames = Vec::new();
 		RecordResult::new(
 			recorder.channel.0.clone(),
 			thread::spawn(move || {
 				while recorder.channel.1.try_recv().is_err() {
-					tick = if tick >= 0. {
-						recorder.clock.get_fps(TimeUnit::Millisecond)
-							- ((tick / 1e6) as f64).round() as f32
-					} else {
-						recorder.clock.get_fps(TimeUnit::Millisecond)
-					};
-					println!("{}", tick);
-					frames
-						.push(Frame::new(get_image().unwrap(), (tick / 10.) as u16));
-					tick = recorder.clock.tick();
+					frames.push(Frame::new(
+						get_image().unwrap(),
+						(recorder.clock.get_fps(TimeUnit::Millisecond) / 10.) as u16,
+					));
+					recorder.clock.tick();
 				}
 				frames
 			}),
