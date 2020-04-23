b642fc1158d4c8d45cd83e1b0fc7843fc62a0382
orhun <orhunparmaksiz@gmail.com>
2020-04-23 22:09:36 +0300

    feat: Add exec_cmd command to util module

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,6 @@
 use clap::{App, ArgMatches};
+use std::io::Error;
+use std::process::Command;
 
 /**
  * Parse command line arguments.
@@ -11,4 +13,14 @@ pub fn parse_args() -> ArgMatches<'static> {
 		.author(env!("CARGO_PKG_AUTHORS"))
 		.about(env!("CARGO_PKG_DESCRIPTION"))
 		.get_matches()
+}
+
+pub fn exec_cmd(cmd: &str, cmd_args: &[&str]) -> Result<(), Error> {
+	match Command::new(cmd).args(cmd_args).spawn() {
+		Ok(mut child) => {
+			child.wait()?;
+			Ok(())
+		}
+		Err(e) => Err(e),
+	}
 }
