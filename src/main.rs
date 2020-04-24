cff95e199144cec89cc2878dee9233726c9fac78
orhun <orhunparmaksiz@gmail.com>
2020-04-24 18:31:48 +0300

    feat: Improve the error handling in main function

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ mod image;
 mod record;
 mod util;
 mod x11;
-
 use self::image::gif::Gif;
 use self::image::Capture;
 use self::record::Recorder;
@@ -11,33 +10,36 @@ use gif::Repeat;
 use std::fs::File;
 use std::process::Command;
 
-fn main() {
+fn main() -> Result<(), std::io::Error> {
 	let _args = util::parse_args();
 	println!("thank god it's friday");
 
-	let display = Display::open().expect("Cannot open display.");
+	let display = Display::open().expect("Cannot open display");
 	let mut focused_window = display.get_focused_window();
 	focused_window.reset_position();
 	let mut gif = Gif::new(
-		File::create("target/test.gif").unwrap(),
+		File::create("target/test.gif").expect("Failed to create file"),
 		focused_window.geometry,
 		15,
 		Repeat::Infinite,
-	)
-	.unwrap();
+	)?;
 
 	let recorder = Recorder::new(20);
 	let record = recorder.record(move || focused_window.get_image());
 
 	match Command::new("kmon").spawn() {
 		Ok(mut child) => {
-			child.wait().unwrap();
+			child.wait()?;
 		}
 		Err(_) => panic!("Failed to run the command."),
 	}
-
-	record.finish().unwrap();
-	let frames = record.thread.join().unwrap();
-	println!("frames: {}", frames.len());
-	gif.save(frames).unwrap();
+	record.finish().expect("Failed to finish the recording");
+	match record.thread.join() {
+		Ok(frames) => {
+			println!("frames: {}", frames.len());
+			gif.save(frames)?;
+		}
+		Err(_) => {panic!("Failed to retrieve the frames")}
+	};
+	Ok(())
 }
