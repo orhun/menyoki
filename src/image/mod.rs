167c315c45b1fa62dba730705a6dd4e069a64c9e
orhun <orhunparmaksiz@gmail.com>
2020-04-29 13:40:51 +0300

    feat: Add constructor for the Geometry struct

--- src/image/mod.rs
+++ src/image/mod.rs
@@ -9,6 +9,17 @@ pub struct Geometry {
 	pub height: u32,
 }
 
+impl Geometry {
+	pub fn new(x: i32, y: i32, width: u32, height: u32) -> Self {
+		Self {
+			x,
+			y,
+			width,
+			height,
+		}
+	}
+}
+
 /* BGR color fields (and padding for XImage casting) */
 #[derive(Debug)]
 pub struct Bgr {
