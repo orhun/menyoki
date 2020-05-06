17bcf9c859c56f0897fe71c5b541f8d8947e2f89
orhun <orhunparmaksiz@gmail.com>
2020-05-07 00:02:36 +0300

    feat: Add display field and constructor method to Overlay

--- src/x11/overlay.rs
+++ src/x11/overlay.rs
@@ -1,6 +1,13 @@
+use crate::x11::display::Display;
 use crate::x11::window::Window;
 
-#[derive(Debug)]
 struct Overlay {
+	display: Display,
 	window: Window,
+}
+
+impl Overlay {
+	pub fn new(display: Display, window: Window) -> Self {
+		Self { display, window }
+	}
 }
