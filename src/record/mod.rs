0eeb154a0c887bcfdb86d083858b3414be418524
orhun <orhunparmaksiz@gmail.com>
2020-05-14 02:19:39 +0300

    fix: Make sender of Record private field

--- src/record/mod.rs
+++ src/record/mod.rs
@@ -11,7 +11,7 @@ use std::time::Duration;
 /* Sender and main thread of the Recorder */
 #[derive(Debug)]
 pub struct Record {
-	pub sender: mpsc::Sender<()>,
+	sender: mpsc::Sender<()>,
 	pub thread: thread::JoinHandle<Vec<Frame>>,
 }
 
