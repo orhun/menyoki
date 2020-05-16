b55904386e32934a4ee4bb56edc953b4abca6cd1
orhun <orhunparmaksiz@gmail.com>
2020-05-16 21:12:17 +0300

    refactor: Use bool type fields in the DeviceState struct

--- src/util/device.rs
+++ src/util/device.rs
@@ -4,6 +4,8 @@ pub struct DeviceState {
 	state: DevState,
 	mouse: Vec<bool>,
 	keys: Vec<Keycode>,
+	pub mouse_clicked: bool,
+	pub exit_keys_pressed: bool,
 }
 
 impl DeviceState {
@@ -12,21 +14,23 @@ impl DeviceState {
 			state: DevState::new(),
 			mouse: Vec::new(),
 			keys: Vec::new(),
+			mouse_clicked: false,
+			exit_keys_pressed: false,
 		}
 	}
 
-	fn update_state(&mut self) {
+	pub fn update(&mut self) {
 		self.mouse = self.state.get_mouse().button_pressed;
 		self.keys = self.state.get_keys();
+		self.mouse_clicked = self.check_mouse_clicked();
+		self.exit_keys_pressed = self.check_exit_keys_pressed();
 	}
 
-	pub fn get_mouse_clicked(&mut self) -> bool {
-		self.update_state();
+	fn check_mouse_clicked(&mut self) -> bool {
 		self.mouse[1] || self.mouse[3]
 	}
 
-	pub fn get_exit_keys_pressed(&mut self) -> bool {
-		self.update_state();
+	fn check_exit_keys_pressed(&mut self) -> bool {
 		self.keys.contains(&Keycode::Escape)
 			|| (self.keys.contains(&Keycode::LControl)
 				&& self.keys.contains(&Keycode::D))
