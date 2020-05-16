2487308a88bdaf6717a57607a9bbbd57914d0004
orhun <orhunparmaksiz@gmail.com>
2020-05-16 20:51:31 +0300

    refactor: Rename get methods in DeviceState

--- src/util/device.rs
+++ src/util/device.rs
@@ -1,6 +1,6 @@
 use device_query::{DeviceQuery, DeviceState as DevState, Keycode, MouseState};
 
-struct DeviceState {
+pub struct DeviceState {
 	state: DevState,
 	mouse: Vec<bool>,
 	keys: Vec<Keycode>,
@@ -20,12 +20,12 @@ impl DeviceState {
 		self.keys = self.state.get_keys();
 	}
 
-	pub fn mouse_clicked(&mut self) -> bool {
+	pub fn get_mouse_clicked(&mut self) -> bool {
 		self.update();
 		self.mouse[1] && self.mouse[3]
 	}
 
-	pub fn exit_keys_pressed(&mut self) -> bool {
+	pub fn get_exit_keys_pressed(&mut self) -> bool {
 		self.update();
 		self.keys.contains(&Keycode::Escape)
 			|| (self.keys.contains(&Keycode::LControl)
