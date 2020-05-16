26f89d69d83e777d9b00109c1a357533ac933606
orhun <orhunparmaksiz@gmail.com>
2020-05-16 20:52:11 +0300

    refactor: Rename update method in DeviceState to update_state

--- src/util/device.rs
+++ src/util/device.rs
@@ -15,18 +15,18 @@ impl DeviceState {
 		}
 	}
 
-	fn update(&mut self) {
+	fn update_state(&mut self) {
 		self.mouse = self.state.get_mouse().button_pressed;
 		self.keys = self.state.get_keys();
 	}
 
 	pub fn get_mouse_clicked(&mut self) -> bool {
-		self.update();
+		self.update_state();
 		self.mouse[1] && self.mouse[3]
 	}
 
 	pub fn get_exit_keys_pressed(&mut self) -> bool {
-		self.update();
+		self.update_state();
 		self.keys.contains(&Keycode::Escape)
 			|| (self.keys.contains(&Keycode::LControl)
 				&& self.keys.contains(&Keycode::D))
