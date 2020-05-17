25791e4db8cf71344ccb42540629ac47b9b86e02
orhun <orhunparmaksiz@gmail.com>
2020-05-17 23:56:33 +0300

    feat: Add WindowSystem struct for handling x11 related events

--- src/x11/mod.rs
+++ src/x11/mod.rs
@@ -1,2 +1,39 @@
 pub mod display;
 pub mod window;
+
+use crate::image::{Capture, Image};
+use crate::util::device::DeviceState;
+use crate::x11::display::Display;
+use clap::ArgMatches;
+
+pub struct WindowSystem {
+	display: Display,
+}
+
+impl WindowSystem {
+	pub fn init() -> Option<Self> {
+		if let Some(display) = Display::open() {
+			Some(Self { display })
+		} else {
+			None
+		}
+	}
+
+	pub fn get_record_func(
+		&mut self,
+		args: ArgMatches,
+	) -> impl Fn() -> Option<Image> {
+		let mut focused_window = self.display.get_focused_window();
+		if !args.is_present("command") {
+			focused_window = self
+				.display
+				.select_focused_window(
+					DeviceState::new(),
+					focused_window.create_gc(0x00ff_ffff),
+				)
+				.unwrap();
+		}
+		focused_window.reset_position();
+		move || focused_window.get_image()
+	}
+}
