570a5b112e319631498db2a8c692910a76df5c77
orhun <orhunparmaksiz@gmail.com>
2020-05-18 02:48:04 +0300

    feat: Add fern logger and its initializer method

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,6 +1,10 @@
 pub mod cmd;
 pub mod device;
+use chrono::Local;
 use clap::{App, Arg, ArgMatches, SubCommand};
+use fern::colors::{Color, ColoredLevelConfig};
+use fern::Dispatch;
+use log::{LevelFilter, SetLoggerError};
 
 /**
  * Parse command line arguments.
@@ -77,6 +81,29 @@ pub fn parse_args() -> ArgMatches<'static> {
 				),
 		)
 		.get_matches()
+}
+
+pub fn init_logger() -> Result<(), SetLoggerError> {
+	let colors = ColoredLevelConfig::new()
+		.info(Color::Magenta)
+		.error(Color::Red)
+		.warn(Color::Yellow)
+		.debug(Color::Blue)
+		.trace(Color::BrightBlack);
+	Dispatch::new()
+		.format(move |out, message, record| {
+			out.finish(format_args!(
+				"[{} {} {}] {}",
+				Local::now().format("%Y-%m-%dT%H:%M:%S"),
+				colors.color(record.level()),
+				record.target(),
+				message
+			))
+		})
+		.level(LevelFilter::Debug)
+		.chain(std::io::stdout())
+		.apply()?;
+	Ok(())
 }
 
 /**
