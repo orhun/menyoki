592c08e395589232bc11306b5f40bd62541a0c47
orhun <orhunparmaksiz@gmail.com>
2020-05-18 19:36:41 +0300

    feat: Set x11 error handler in WindowSystem initializer

--- src/x11/mod.rs
+++ src/x11/mod.rs
@@ -5,6 +5,9 @@ use crate::app::AppSettings;
 use crate::image::{Capture, Image};
 use crate::util::device::DeviceState;
 use crate::x11::display::Display;
+use std::ffi::CStr;
+use std::os::raw::c_char;
+use x11::xlib;
 
 pub struct WindowSystem {
 	display: Display,
@@ -13,6 +16,7 @@ pub struct WindowSystem {
 impl WindowSystem {
 	pub fn init() -> Option<Self> {
 		if let Some(display) = Display::open() {
+			unsafe { xlib::XSetErrorHandler(Some(x11_error_handler)) };
 			Some(Self { display })
 		} else {
 			None
@@ -36,4 +40,33 @@ impl WindowSystem {
 		focused_window.reset_position();
 		move || focused_window.get_image()
 	}
+}
+
+unsafe extern "C" fn x11_error_handler(
+	display: *mut xlib::Display,
+	error: *mut xlib::XErrorEvent,
+) -> i32 {
+	let mut error_msg = String::from("X Error");
+	let mut error_text: Vec<u8> = Vec::with_capacity(1024);
+	if xlib::XGetErrorText(
+		display,
+		(*error).error_code as i32,
+		error_text.as_mut_ptr() as *mut c_char,
+		error_text.capacity() as i32,
+	) == 0
+	{
+		error_msg += &format!(
+			": {}",
+			CStr::from_ptr(error_text.as_mut_ptr() as *mut c_char)
+				.to_string_lossy()
+				.into_owned()
+		);
+	}
+	error!(
+		"{} Opcode: {}, Serial: {}",
+		error_msg,
+		(*error).request_code,
+		(*error).serial
+	);
+	0
 }
