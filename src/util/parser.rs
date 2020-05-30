8634a0eb86439404ef20d5d2ee3be3bb84faa199
orhun <orhunparmaksiz@gmail.com>
2020-05-31 00:02:32 +0300

    doc: Add comment to ArgParser struct fields and methods

--- src/util/parser.rs
+++ src/util/parser.rs
@@ -1,16 +1,30 @@
 use clap::ArgMatches;
 use std::str::FromStr;
 
+/* Clap argument parser */
 #[derive(Debug)]
 pub struct ArgParser<'a> {
 	pub args: &'a ArgMatches<'a>,
 }
 
 impl<'a> ArgParser<'a> {
+	/**
+	 * Create a new ArgParser object.
+	 *
+	 * @param  args
+	 * @return ArgParser
+	 */
 	pub fn new(args: &'a ArgMatches<'a>) -> Self {
 		Self { args }
 	}
 
+	/**
+	 * Parse an argument and return the parsed value.
+	 *
+	 * @param  arg
+	 * @param  default_value
+	 * @return T
+	 */
 	pub fn parse<T: FromStr>(&self, arg: &str, default_value: T) -> T {
 		self.args
 			.value_of(arg)
