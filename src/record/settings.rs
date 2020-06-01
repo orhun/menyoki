1096d710a0e48c559c891cb9e18a8c088e502ddf
orhun <orhunparmaksiz@gmail.com>
2020-06-01 20:28:55 +0300

    refactor: Use default values from Default implementation for RecordSettings

--- src/record/settings.rs
+++ src/record/settings.rs
@@ -13,6 +13,21 @@ pub struct RecordSettings {
 	pub record_root: bool,
 }
 
+/* Default initialization values for RecordSettings */
+impl Default for RecordSettings {
+	fn default() -> Self {
+		Self {
+			fps: 10,
+			padding: 5,
+			timeout: 30,
+			interval: 10,
+			countdown: 3,
+			color: 0x00ff_00ff,
+			record_root: false,
+		}
+	}
+}
+
 impl RecordSettings {
 	/**
 	 * Create a new RecordSettings object.
@@ -58,11 +73,11 @@ impl RecordSettings {
 			Some(matches) => {
 				let parser = ArgParser::new(&matches);
 				Self::new(
-					parser.parse::<u32>("fps", 10),
-					parser.parse::<u32>("padding", 5),
-					parser.parse::<u64>("timeout", 30),
-					parser.parse::<u64>("interval", 10),
-					parser.parse::<u64>("countdown", 3),
+					parser.parse::<u32>("fps", Self::default().fps),
+					parser.parse::<u32>("padding", Self::default().padding),
+					parser.parse::<u64>("timeout", Self::default().timeout),
+					parser.parse::<u64>("interval", Self::default().interval),
+					parser.parse::<u64>("countdown", Self::default().countdown),
 					color,
 					matches.is_present("root"),
 				)
@@ -72,21 +87,6 @@ impl RecordSettings {
 				settings.color = color;
 				settings
 			}
-		}
-	}
-}
-
-/* Default initialization values for RecordSettings */
-impl Default for RecordSettings {
-	fn default() -> Self {
-		Self {
-			fps: 10,
-			padding: 5,
-			timeout: 30,
-			interval: 10,
-			countdown: 3,
-			color: 0x00ff_00ff,
-			record_root: false,
 		}
 	}
 }
