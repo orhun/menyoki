1d4503b457d3949de096f72ce2bd49160d8be8f9
orhun <orhunparmaksiz@gmail.com>
2020-06-03 16:52:32 +0300

    fix: Fix the usage of subcommands while argument parsing

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -13,6 +13,113 @@ use log::{LevelFilter, SetLoggerError};
  * @return ArgMatches
  */
 pub fn parse_args() -> ArgMatches<'static> {
+	let save_settings = SubCommand::with_name("save")
+		.about("Changes the output file settings")
+		.arg(
+			Arg::with_name("output")
+				.value_name("FILE")
+				.default_value("t.gif")
+				.help("Sets the output file"),
+		)
+		.arg(
+			Arg::with_name("date")
+				.short("d")
+				.long("date")
+				.help("Adds date and time to the file name"),
+		)
+		.arg(
+			Arg::with_name("timestamp")
+				.short("t")
+				.long("timestamp")
+				.help("Adds timestamp to the file name"),
+		)
+		.arg(
+			Arg::with_name("prompt")
+				.short("p")
+				.long("prompt")
+				.help("Shows prompt for the file name input"),
+		);
+	let gif_settings = SubCommand::with_name("gif")
+		.about("Changes the GIF encoder settings")
+		.arg(
+			Arg::with_name("speed")
+				.short("s")
+				.long("speed")
+				.value_name("SPEED")
+				.default_value("10")
+				.help("Sets the frame encoding speed (1-30)")
+				.takes_value(true),
+		)
+		.arg(
+			Arg::with_name("repeat")
+				.short("r")
+				.long("repeat")
+				.value_name("REPEAT")
+				.help("Sets the number of repetitions [default: \u{221E}]")
+				.takes_value(true),
+		)
+		.subcommand(save_settings);
+	let record_settings = SubCommand::with_name("record")
+		.about("Changes the recording settings")
+		.arg(
+			Arg::with_name("root")
+				.short("r")
+				.long("root")
+				.help("Records the root window"),
+		)
+		.arg(
+			Arg::with_name("focus")
+				.short("w")
+				.long("focus")
+				.conflicts_with("root")
+				.help("Records the focus window"),
+		)
+		.arg(
+			Arg::with_name("countdown")
+				.short("c")
+				.long("countdown")
+				.value_name("S")
+				.default_value("3")
+				.help("Sets the countdown value for recording")
+				.takes_value(true),
+		)
+		.arg(
+			Arg::with_name("fps")
+				.short("f")
+				.long("fps")
+				.value_name("FPS")
+				.default_value("10")
+				.help("Sets the FPS (frames per second) value")
+				.takes_value(true),
+		)
+		.arg(
+			Arg::with_name("interval")
+				.short("i")
+				.long("interval")
+				.value_name("MS")
+				.default_value("10")
+				.help("Sets the interval time for window selection")
+				.takes_value(true),
+		)
+		.arg(
+			Arg::with_name("border")
+				.short("b")
+				.long("border")
+				.value_name("BORDER")
+				.default_value("5")
+				.help("Sets the border padding value")
+				.takes_value(true),
+		)
+		.arg(
+			Arg::with_name("timeout")
+				.short("t")
+				.long("timeout")
+				.value_name("S")
+				.default_value("30")
+				.help("Sets the timeout for window selection")
+				.takes_value(true),
+		)
+		.subcommand(gif_settings);
 	App::new(env!("CARGO_PKG_NAME"))
 		.version(env!("CARGO_PKG_VERSION"))
 		.author(env!("CARGO_PKG_AUTHORS"))
@@ -31,117 +138,7 @@ pub fn parse_args() -> ArgMatches<'static> {
 				.help("Sets the main color to use")
 				.takes_value(true),
 		)
-		.subcommand(
-			SubCommand::with_name("record")
-				.about("Changes the recording settings")
-				.arg(
-					Arg::with_name("root")
-						.short("r")
-						.long("root")
-						.help("Records the root window"),
-				)
-				.arg(
-					Arg::with_name("focus")
-						.short("w")
-						.long("focus")
-						.conflicts_with("root")
-						.help("Records the focus window"),
-				)
-				.arg(
-					Arg::with_name("countdown")
-						.short("c")
-						.long("countdown")
-						.value_name("S")
-						.default_value("3")
-						.help("Sets the countdown value for recording")
-						.takes_value(true),
-				)
-				.arg(
-					Arg::with_name("fps")
-						.short("f")
-						.long("fps")
-						.value_name("FPS")
-						.default_value("10")
-						.help("Sets the FPS (frames per second) value")
-						.takes_value(true),
-				)
-				.arg(
-					Arg::with_name("interval")
-						.short("i")
-						.long("interval")
-						.value_name("MS")
-						.default_value("10")
-						.help("Sets the interval time for window selection")
-						.takes_value(true),
-				)
-				.arg(
-					Arg::with_name("border")
-						.short("b")
-						.long("border")
-						.value_name("BORDER")
-						.default_value("5")
-						.help("Sets the border padding value")
-						.takes_value(true),
-				)
-				.arg(
-					Arg::with_name("timeout")
-						.short("t")
-						.long("timeout")
-						.value_name("S")
-						.default_value("30")
-						.help("Sets the timeout for window selection")
-						.takes_value(true),
-				),
-		)
-		.subcommand(
-			SubCommand::with_name("gif")
-				.about("Changes the GIF encoder settings")
-				.arg(
-					Arg::with_name("speed")
-						.short("s")
-						.long("speed")
-						.value_name("SPEED")
-						.default_value("10")
-						.help("Sets the frame encoding speed (1-30)")
-						.takes_value(true),
-				)
-				.arg(
-					Arg::with_name("repeat")
-						.short("r")
-						.long("repeat")
-						.value_name("REPEAT")
-						.help("Sets the number of repetitions [default: \u{221E}]")
-						.takes_value(true),
-				),
-		)
-		.subcommand(
-			SubCommand::with_name("save")
-				.about("Changes the output file settings")
-				.arg(
-					Arg::with_name("output")
-						.value_name("FILE")
-						.default_value("t.gif")
-						.help("Sets the output file"),
-				)
-				.arg(
-					Arg::with_name("date")
-						.short("d")
-						.long("date")
-						.help("Adds date and time to the file name"),
-				)
-				.arg(
-					Arg::with_name("timestamp")
-						.short("t")
-						.long("timestamp")
-						.help("Adds timestamp to the file name"),
-				)
-				.arg(
-					Arg::with_name("prompt")
-						.short("p")
-						.long("prompt")
-						.help("Shows prompt for the file name input"),
-				),
-		)
+		.subcommand(record_settings)
 		.get_matches()
 }
 
