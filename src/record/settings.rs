af01bcb2f27c30eea1e68566b92842b56849c41a
orhun <orhunparmaksiz@gmail.com>
2020-06-05 22:32:53 +0300

    refactor: Create RecordTime struct for storing time related settings

--- src/record/settings.rs
+++ src/record/settings.rs
@@ -9,16 +9,51 @@ pub enum RecordWindow {
 	None,
 }
 
+/* Time related recording settings */
+#[derive(Clone, Copy, Debug)]
+pub struct RecordTime {
+	pub timeout: u64,
+	pub interval: u64,
+	pub countdown: u64,
+}
+
+impl RecordTime {
+	/**
+	 * Create a new RecordTime object.
+	 *
+	 * @param  timeout
+	 * @param  interval
+	 * @param  countdown
+	 * @return RecordTime
+	 */
+	pub fn new(timeout: u64, interval: u64, countdown: u64) -> Self {
+		Self {
+			timeout,
+			interval,
+			countdown,
+		}
+	}
+}
+
+/* Default initialization values for RecordTime */
+impl Default for RecordTime {
+	fn default() -> Self {
+		Self {
+			timeout: 30,
+			interval: 10,
+			countdown: 3,
+		}
+	}
+}
+
 /* Recording and window settings */
 #[derive(Clone, Copy, Debug)]
 pub struct RecordSettings {
 	pub fps: u32,
-	pub border: u32,
-	pub timeout: u64,
-	pub interval: u64,
-	pub countdown: u64,
 	pub color: u64,
+	pub border: u32,
 	pub padding: Padding,
+	pub time: RecordTime,
 	pub window: RecordWindow,
 }
 
@@ -27,12 +62,10 @@ impl Default for RecordSettings {
 	fn default() -> Self {
 		Self {
 			fps: 10,
-			border: 5,
-			timeout: 30,
-			interval: 10,
-			countdown: 3,
 			color: 0x00ff_00ff,
+			border: 5,
 			padding: Padding::default(),
+			time: RecordTime::default(),
 			window: RecordWindow::None,
 		}
 	}
@@ -43,33 +76,27 @@ impl RecordSettings {
 	 * Create a new RecordSettings object.
 	 *
 	 * @param  fps
-	 * @param  border
-	 * @param  timeout
-	 * @param  interval
-	 * @param  countdown
 	 * @param  color
+	 * @param  border
 	 * @param  padding
+	 * @param  time
 	 * @param  window
 	 * @return RecordSettings
 	 */
 	pub fn new(
 		fps: u32,
-		border: u32,
-		timeout: u64,
-		interval: u64,
-		countdown: u64,
 		color: u64,
+		border: u32,
 		padding: Padding,
+		time: RecordTime,
 		window: RecordWindow,
 	) -> Self {
 		Self {
 			fps,
-			border,
-			timeout,
-			interval,
-			countdown,
 			color,
+			border,
 			padding,
+			time,
 			window,
 		}
 	}
@@ -84,16 +111,18 @@ impl RecordSettings {
 		match parser.args {
 			Some(matches) => Self::new(
 				parser.parse("fps", Self::default().fps),
-				parser.parse("border", Self::default().border),
-				parser.parse("timeout", Self::default().timeout),
-				parser.parse("interval", Self::default().interval),
-				parser.parse("countdown", Self::default().countdown),
 				u64::from_str_radix(
 					matches.value_of("color").unwrap_or_default(),
 					16,
 				)
 				.unwrap_or(Self::default().color),
+				parser.parse("border", Self::default().border),
 				Padding::parse(matches.value_of("padding").unwrap_or_default()),
+				RecordTime::new(
+					parser.parse("timeout", Self::default().time.timeout),
+					parser.parse("interval", Self::default().time.interval),
+					parser.parse("countdown", Self::default().time.countdown),
+				),
 				if matches.is_present("focus") {
 					RecordWindow::Focus
 				} else if matches.is_present("root") {
