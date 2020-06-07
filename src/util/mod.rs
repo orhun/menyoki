4620b06c776194e67c5faffc8f5e0badfe4c8959
orhun <orhunparmaksiz@gmail.com>
2020-06-07 17:16:36 +0300

    fix: Remove unused functions from util module

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,5 +1,6 @@
 pub mod cmd;
 pub mod device;
+pub mod settings;
 use chrono::Local;
 use fern::colors::{Color, ColoredLevelConfig};
 use fern::Dispatch;
@@ -33,37 +34,9 @@ pub fn init_logger() -> Result<(), SetLoggerError> {
 	Ok(())
 }
 
-/**
- * Append the given information to the file name.
- *
- * @param  file_name
- * @param  info
- * @return String
- */
-pub fn update_file_name(file_name: String, info: String) -> String {
-	file_name
-		.split('.')
-		.enumerate()
-		.map(|(i, s)| {
-			if i == 0 {
-				format!("{}_{}", s, info)
-			} else {
-				format!(".{}", s)
-			}
-		})
-		.collect::<String>()
-}
-
 #[cfg(test)]
 mod tests {
 	use super::*;
-	#[test]
-	fn test_update_file_name() {
-		assert_eq!(
-			"t_1588101718.gif",
-			update_file_name(String::from("t.gif"), String::from("1588101718"))
-		)
-	}
 	#[test]
 	fn test_init_logger() -> Result<(), SetLoggerError> {
 		init_logger()?;
