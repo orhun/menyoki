a26347a0a28159eef267548c97b6b2f62cc2103c
orhun <orhunparmaksiz@gmail.com>
2020-06-16 19:20:21 +0300

    fix: Use checked methods for mathematical operations in Geometry

--- src/image/geometry.rs
+++ src/image/geometry.rs
@@ -30,10 +30,16 @@ impl Geometry {
 	) -> Self {
 		let padding = padding.unwrap_or_default();
 		Self {
-			x: x + i32::try_from(padding.right).unwrap_or_default(),
-			y: y + i32::try_from(padding.top).unwrap_or_default(),
-			width: width - (padding.right + padding.left),
-			height: height - (padding.top + padding.bottom),
+			x: x.checked_add(i32::try_from(padding.right).unwrap_or_default())
+				.unwrap_or_default(),
+			y: y.checked_add(i32::try_from(padding.top).unwrap_or_default())
+				.unwrap_or_default(),
+			width: width
+				.checked_sub(padding.right + padding.left)
+				.unwrap_or_default(),
+			height: height
+				.checked_sub(padding.top + padding.bottom)
+				.unwrap_or_default(),
 		}
 	}
 }
