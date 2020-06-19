a9d40ccb2e8fa215a77ae00c9ec0cda92802fcf0
orhun <orhunparmaksiz@gmail.com>
2020-06-19 19:12:31 +0300

    feat: Create File module in util for handling the output file

---
+++ src/util/file.rs
@@ -0,0 +1,114 @@
+use chrono::Local;
+use clap::ArgMatches;
+use std::fmt;
+
+/* Information to include in file name */
+#[derive(Clone, Copy, Debug)]
+pub enum FileInfo {
+	Date,
+	Timestamp,
+}
+
+impl FileInfo {
+	/**
+	 * Create a FileInfo enum from parsed arguments.
+	 *
+	 * @param  args
+	 * @return FileInfo (Option)
+	 */
+	pub fn from_args(args: &ArgMatches<'_>) -> Option<Self> {
+		if args.is_present("date") {
+			Some(Self::Date)
+		} else if args.is_present("timestamp") {
+			Some(Self::Timestamp)
+		} else {
+			None
+		}
+	}
+}
+
+/* Display implementation for user-facing output */
+impl fmt::Display for FileInfo {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		write!(
+			f,
+			"{}",
+			match self {
+				FileInfo::Date => Local::now().format("%Y%m%dT%H%M%S").to_string(),
+				FileInfo::Timestamp => Local::now().timestamp().to_string(),
+			}
+		)
+	}
+}
+
+/* Format of the output file */
+#[derive(Debug)]
+pub enum FileFormat {
+	Gif,
+	Png,
+	Jpg,
+}
+
+/* Display implementation for user-facing output */
+impl fmt::Display for FileFormat {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		write!(f, "{:?}", self)
+	}
+}
+
+impl FileFormat {
+	/**
+	 * Create a FileFormat enum fron parsed arguments.
+	 *
+	 * @param  args
+	 * @return FileFormat
+	 */
+	pub fn from_args<'a>(args: &'a ArgMatches<'a>) -> Self {
+		match args.subcommand_matches("capture") {
+			Some(matches) => {
+				if matches.is_present("jpg") {
+					Self::Jpg
+				} else {
+					Self::Png
+				}
+			}
+			None => Self::Gif,
+		}
+	}
+}
+
+/* Representation of the output file */
+#[derive(Debug)]
+pub struct File {
+	pub name: String,
+	pub format: FileFormat,
+	pub info: Option<FileInfo>,
+}
+
+impl File {
+	/**
+	 * Create a new File object.
+	 *
+	 * @param  name
+	 * @param  format
+	 * @param  info
+	 * @return File
+	 */
+	pub fn new(name: String, format: FileFormat, info: Option<FileInfo>) -> Self {
+		Self { name, format, info }
+	}
+
+	/**
+	 * Create a new File object from file format.
+	 *
+	 * @param  file_format
+	 * @return File
+	 */
+	pub fn from_format(file_format: FileFormat) -> Self {
+		Self::new(
+			format!("t.{}", file_format.to_string().to_lowercase()),
+			file_format,
+			None,
+		)
+	}
+}
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,4 +1,5 @@
 pub mod cmd;
+pub mod file;
 pub mod modifier;
 pub mod settings;
 pub mod state;
