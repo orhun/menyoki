2615a2e1bc4a1a43e89aacc7c7a04208e6ac9371
orhun <orhunparmaksiz@gmail.com>
2020-06-22 00:31:32 +0300

    feat: Use alpha field for the Bgra struct

--- src/image/bgra.rs
+++ src/image/bgra.rs
@@ -4,7 +4,7 @@ pub struct Bgra {
 	b: u8,
 	g: u8,
 	r: u8,
-	a: u8,
+	alpha: u8,
 }
 
 impl Bgra {
@@ -14,23 +14,30 @@ impl Bgra {
 	 * @param  b
 	 * @param  g
 	 * @param  r
+	 * @param  alpha
 	 * @return Bgra
 	 */
 	#[allow(dead_code)]
-	pub fn new(b: u8, g: u8, r: u8) -> Self {
-		Self { b, g, r, a: 0 }
+	pub fn new(b: u8, g: u8, r: u8, alpha: u8) -> Self {
+		Self { b, g, r, alpha }
 	}
 
 	/**
-	 * Convert a BGR slice to RGB pixel vector.
+	 * Convert a BGRA slice to RGBA pixel vector.
 	 *
 	 * @param  bgra_data
+	 * @param  alpha
 	 * @return Vector of u8
 	 */
-	pub fn get_rgb_pixels(bgra_data: &[Bgra]) -> Vec<u8> {
+	pub fn get_rgba_pixels(bgra_data: &[Bgra], alpha: bool) -> Vec<u8> {
 		let mut pixels = Vec::new();
-		for bgr in bgra_data {
-			pixels.extend(&[bgr.r, bgr.g, bgr.b])
+		for bgra in bgra_data {
+			pixels.extend(&[
+				bgra.r,
+				bgra.g,
+				bgra.b,
+				if alpha { bgra.alpha } else { 255 },
+			]);
 		}
 		pixels
 	}
