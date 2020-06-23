99c26e601a88a5bcb6641b3c24c0707dc38c303f
orhun <orhunparmaksiz@gmail.com>
2020-06-24 00:25:43 +0300

    feat: Create get_data method in Image for getting the data in given color

--- src/image/mod.rs
+++ src/image/mod.rs
@@ -4,11 +4,13 @@ pub mod padding;
 pub mod settings;
 
 use crate::image::geometry::Geometry;
+use image::{Bgra, ColorType};
 
 /* Image data and geometric properties */
 #[derive(Clone, Debug)]
 pub struct Image {
-	pub data: Vec<u8>,
+	data: Vec<Bgra<u8>>,
+	alpha_channel: bool,
 	pub geometry: Geometry,
 }
 
@@ -17,11 +19,41 @@ impl Image {
 	 * Create a new Image object.
 	 *
 	 * @param  data
+	 * @param  alpha_channel
 	 * @param  geometry
 	 * @return Image
 	 */
-	pub fn new(data: Vec<u8>, geometry: Geometry) -> Self {
-		Self { data, geometry }
+	pub fn new(
+		data: Vec<Bgra<u8>>,
+		alpha_channel: bool,
+		geometry: Geometry,
+	) -> Self {
+		Self {
+			data,
+			alpha_channel,
+			geometry,
+		}
+	}
+
+	/**
+	 * Get image data in the given color type.
+	 *
+	 * @param  color_type
+	 * @return Vector of u8
+	 */
+	pub fn get_data(&self, color_type: ColorType) -> Vec<u8> {
+		self.data.iter().fold(Vec::<u8>::new(), |mut rgba8, bgra| {
+			let alpha = if self.alpha_channel { bgra[3] } else { 255 };
+			rgba8.extend(&match color_type {
+				ColorType::Rgb8 => vec![bgra[2], bgra[1], bgra[0]],
+				ColorType::Rgba16 => vec![
+					bgra[2], bgra[2], bgra[1], bgra[1], bgra[0], bgra[0], alpha,
+					alpha,
+				],
+				_ => vec![bgra[2], bgra[1], bgra[0], alpha],
+			});
+			rgba8
+		})
 	}
 }
 
