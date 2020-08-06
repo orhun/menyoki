b2561527a21765b44b8bfdaa66775f3bad99dd38
orhun <orhunparmaksiz@gmail.com>
2020-08-07 02:08:29 +0300

    feat: Show the percentage while processing frames

--- src/gif/decoder.rs
+++ src/gif/decoder.rs
@@ -4,7 +4,7 @@ use crate::gif::settings::GifSettings;
 use image::error::ImageError;
 use image::gif::GifDecoder;
 use image::AnimationDecoder;
-use std::io::Read;
+use std::io::{self, Read, Write};
 
 /* GIF decoder and settings */
 pub struct Decoder<'a, Input: Read> {
@@ -40,7 +40,6 @@ impl<'a, Input: Read> Decoder<'a, Input> {
 	 * @return Result
 	 */
 	pub fn update_frames(mut self) -> Result<Frames, ImageError> {
-		info!("Processing the GIF frames...");
 		let frames = self.decoder.into_frames().collect_frames()?;
 		let first_frame = frames.first().expect("No frames found to process");
 		let fps = ((1e3 / first_frame.delay().numer_denom_ms().0 as f32)
@@ -48,9 +47,23 @@ impl<'a, Input: Read> Decoder<'a, Input> {
 		self.imageops
 			.init(first_frame.clone().into_buffer().dimensions());
 		let mut images = Vec::new();
-		for frame in frames {
-			images.push(self.imageops.process(frame.into_buffer()).get_image());
+		for (i, frame) in frames.iter().enumerate() {
+			let percentage = ((i + 1) as f64 / frames.len() as f64) * 100.;
+			info!("Processing the GIF frames... ({:.1}%)\r", percentage);
+			debug!(
+				"Processing the GIF frames... ({:.1}%) [{}/{}]\r",
+				percentage,
+				i + 1,
+				frames.len()
+			);
+			io::stdout().flush().expect("Failed to flush stdout");
+			images.push(
+				self.imageops
+					.process(frame.clone().into_buffer())
+					.get_image(),
+			);
 		}
+		info!("\n");
 		Ok((images, fps))
 	}
 }
