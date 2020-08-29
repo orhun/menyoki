5d7206b0412d23d43f58d05a76eaa9c32604869a
orhun <orhunparmaksiz@gmail.com>
2020-08-29 21:55:02 +0300

    refactor: Create cut_duration method in gif decoder module

--- src/gif/decoder.rs
+++ src/gif/decoder.rs
@@ -4,6 +4,7 @@ use crate::gif::settings::GifSettings;
 use image::error::ImageError;
 use image::gif::GifDecoder;
 use image::AnimationDecoder;
+use image::Frame;
 use std::convert::TryInto;
 use std::io::{self, Read, Write};
 
@@ -36,6 +37,35 @@ impl<'a, Input: Read> Decoder<'a, Input> {
 	}
 
 	/**
+	 * Update frames to cut the duration.
+	 *
+	 * @param  frames
+	 * @param  cut
+	 * @param  fps
+	 * @return Vector of Frame
+	 */
+	fn cut_duration(
+		frames: &mut Vec<Frame>,
+		cut: (f32, f32),
+		fps: u32,
+	) -> Vec<Frame> {
+		if cut != (0., 0.) {
+			let (start, end) = cut;
+			let frame_delay = 1000_u32.checked_div(fps).unwrap_or_default() as f32;
+			frames
+				.drain(
+					((start / frame_delay) as u32)
+						.try_into()
+						.unwrap_or_default()
+						..frames.len().saturating_sub((end / frame_delay) as usize),
+				)
+				.collect()
+		} else {
+			frames.to_vec()
+		}
+	}
+
+	/**
 	 * Update and return the frames.
 	 *
 	 * @return Result
@@ -43,22 +73,11 @@ impl<'a, Input: Read> Decoder<'a, Input> {
 	pub fn update_frames(mut self) -> Result<Frames, ImageError> {
 		let mut frames = self.decoder.into_frames().collect_frames()?;
 		let first_frame = frames.first().expect("No frames found to process");
-		let fps = ((1e3 / first_frame.delay().numer_denom_ms().0 as f32)
-			* self.settings.speed) as u32;
 		self.imageops
 			.init(first_frame.clone().into_buffer().dimensions());
-		if self.settings.cut != (0., 0.) {
-			let (start, end) = self.settings.cut;
-			let frame_delay = 1000_u32.checked_div(fps).unwrap_or_default() as f32;
-			frames = frames
-				.drain(
-					((start / frame_delay) as u32)
-						.try_into()
-						.unwrap_or_default()
-						..frames.len().saturating_sub((end / frame_delay) as usize),
-				)
-				.collect();
-		}
+		let fps = ((1e3 / first_frame.delay().numer_denom_ms().0 as f32)
+			* self.settings.speed) as u32;
+		let frames = Self::cut_duration(&mut frames, self.settings.cut, fps);
 		let mut images = Vec::new();
 		for (i, frame) in frames.iter().enumerate() {
 			let percentage = ((i + 1) as f64 / frames.len() as f64) * 100.;
