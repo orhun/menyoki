2dec6c71ab32f25d228a85e21e39014aacd7cd02
orhun <orhunparmaksiz@gmail.com>
2020-11-08 15:37:32 +0300

    chore: Add shell completion generator script

---
+++ completions/generate.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+metadata=$(cargo metadata --no-deps --format-version 1)
+bin=$(jq -r '.packages[0].name' <<< "$metadata")
+target=$(jq -r '.target_directory' <<< "$metadata")
+workspace=$(jq -r '.workspace_root' <<< "$metadata")
+echo "==> Building the project..."
+cargo build --release --quiet
+echo "==> Generating shell completions..."
+for sh in "bash" "fish" "zsh"; do
+    "$target/release/$bin" misc -g $sh > "$workspace/completions/$bin.$sh"
+done
+echo "==> Done."
