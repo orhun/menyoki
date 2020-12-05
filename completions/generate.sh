f3f789adce18fa762ac519ee83589a910bdc728b
orhun <orhunparmaksiz@gmail.com>
2020-12-05 19:45:43 +0300

    chore: Add completions for powershell and elvish

--- completions/generate.sh
+++ completions/generate.sh
@@ -8,7 +8,7 @@ workspace=$(jq -r '.workspace_root' <<< "$metadata")
 echo "==> Building the project..."
 cargo build
 echo "==> Generating shell completions..."
-for sh in "bash" "fish" "zsh"; do
+for sh in "bash" "fish" "zsh" "powershell" "elvish"; do
     "$target/debug/$bin" misc -g $sh > "$workspace/completions/$bin.$sh"
 done
 echo "==> Done."
